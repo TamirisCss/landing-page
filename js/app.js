a0c8c70a108636448063a8591356bcb2ea1fd3c8
Tamiris Cristine <tamiris.css@gmail.com>
2021-08-10 14:06:23 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,42 +1,6 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
- */
-
+//Define Global Variables
 const navList = document.getElementById('navbar__list');
 const sections = document.querySelectorAll('section');
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
 
 // Build menu 
 // build the nav
@@ -58,7 +22,7 @@ for (let section of sections) {
 // Set sections as active
 window.addEventListener('scroll', function(event){
     sections.forEach(element => {
-        element.classList.remove('your-active-class')
+        element.classList.remove('active')
         let a = document.getElementById(`link_${element.id}`);
         a.classList.remove('menu__link_active');
         var position = element.getBoundingClientRect();
@@ -66,7 +30,7 @@ window.addEventListener('scroll', function(event){
         // Add class 'active' to section when near top of viewport
         if(position.top >= (0 - element.clientHeight + 10) && position.bottom <= element.clientHeight + 10) {
             a.classList.add('menu__link_active');
-            element.classList.add('your-active-class');
+            element.classList.add('active');
         }
         
     });
